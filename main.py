2dd26ae79f5444f65724beb0237aa7d280e7cf2b
Tommaso Buonocore <buonocore.tms@gmail.com>
2021-10-25 22:33:13 +0200

    refactoring + icon

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# backend file(s) = python
 import eel
 from transformers import pipeline
 
+# Set NLP pipeline
 model_name = "deepset/roberta-base-squad2"
 nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
 
+# Init eel specifying the directory where the frontend files are located 
 eel.init("web")
 
-# Exposing the function to javascript
+# Any functions decorated with @eel.expose will appear as methods on the eel object on the Javascript side
+# N.B: you can do also the other way around with eel.expose(my_javascript_function)
 @eel.expose
 def load_answer(quest, cont):
     print("Computing Answer...")
@@ -14,5 +18,5 @@ def load_answer(quest, cont):
              "context": str(cont)}
     return nlp(QA_input)
 
-# Start the index.html file
-eel.start("index.html")
+# This will start a webserver on the default settings (http://localhost:8000) and open a browser to http://localhost:8000/index.html.
+eel.start("index.html", size=(1200,800))
--- web/script.js
+++ web/script.js
@@ -1,3 +1,6 @@
+/**
+ * Empty all the fields and values when contextButton is clicked
+ */
 document.querySelector("#contextButton").onclick = function () {
     document.querySelector("#context").value = " ";
     document.querySelector("#context").innerHTML = " ";
@@ -7,6 +10,9 @@ document.querySelector("#contextButton").onclick = function () {
     document.querySelector("#answer").innerHTML = " ";
 }
 
+/**
+ * Populate context and question with predefined text and trigger the qa model by clicking the answer button
+ */
 function runTest() {
     document.querySelector("#context").value = "In Question Answering tasks, the model receives a question regarding text content and is required to mark the beginning and end of the answer in the text. If we have a very large set of such texts together with sample questions and the position of the answers in the text, we can train a neural network to learn relationships between context, questions, and answers. The resulting network would be able to answer unseen questions given new contexts which are similar to the training texts. In general, question answering (QA) is a computer science discipline within the fields of information retrieval and natural language processing (NLP), which is concerned with building systems that automatically answer questions posed by humans in a natural language. Machine reading comprehension has captured the minds of computer scientists for decades. The recent production of large-scale labeled datasets has allowed researchers to build supervised neural systems that automatically answer questions posed in a natural language.";
     document.querySelector("#question").value = "What is a question answering system?";
@@ -14,6 +20,10 @@ function runTest() {
     document.querySelector("#answerButton").click();
   }
 
+/**
+ * When answerButton is clicked, take context and question strings and pass them to the python function that will compute the answer,
+ * than inspect the outcome to retrieve the answer string and the confidence score. Depending on the amount of confidence, change the progress bar color.
+ */
 document.querySelector("#answerButton").onclick = function () {
     var question = document.querySelector("#question").value
     var context = document.querySelector("#context").value
@@ -32,6 +42,9 @@ document.querySelector("#answerButton").onclick = function () {
     })
 }
 
+/**
+ * Add key listener to trigger answer when enter is pressed
+ */
 document.querySelector("#question").addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
    event.preventDefault();
